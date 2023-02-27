4afc236ec78e204ae8228b840aa2141b677bc152
Aleksei Faizullin <aleksei.faizullin@softswiss.com>
2023-02-27 11:30:45 +0700

    chore: update go dependencies

--- template/server/cmd/main.go
+++ template/server/cmd/main.go
@@ -1,125 +1,149 @@
 package main
 
 import (
-  "context"
-  "flag"
-  "fmt"
-  "github.com/gorilla/handlers"
-  "github.com/gorilla/mux"
-  "github.com/improbable-eng/grpc-web/go/grpcweb"
-  oklog "github.com/oklog/run"
-  "google.golang.org/grpc"
-  "log"
-  "net/http"
-  "os"
-  "os/signal"
-  proto "server/proto/hellosvc"
-  "server/src/hellosvc"
-  "strings"
-  "syscall"
-  "time"
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
+	proto "server/proto/hellosvc"
+	"server/src/hellosvc"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"github.com/improbable-eng/grpc-web/go/grpcweb"
+	oklog "github.com/oklog/run"
+	"google.golang.org/grpc"
 )
 
-func run() error {
-  conf := getConfig()
-
-  grpcServer := grpc.NewServer()
-  proto.RegisterHelloServiceServer(grpcServer, hellosvc.NewServiceServer())
-  wrappedGrpc := grpcweb.WrapServer(grpcServer, grpcweb.WithOriginFunc(func(origin string) bool {
-    return origin == conf.originsAllowed
-  }))
-
-  router := mux.NewRouter().PathPrefix("/api").Subrouter()
-
-  // here you can add more routes and handlers
-  {
-    //router.Methods("GET").Path("/healthcheck").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    //  w.WriteHeader(http.StatusOK)
-    //  w.Write([]byte("ok"))
-    //})
-  }
-
-  corsOrigins := handlers.AllowedOrigins([]string{conf.originsAllowed})
-  corsHeaders := handlers.AllowedHeaders([]string{"Accept", "Content-Type", "Content-Length", "Accept-Encoding", "Authorization", "X-CSRF-Token", "array", "arrayindexoffset_", "convertedprimitivefields_", "messageid_", "pivot_", "wrappers_", "x-grpc-web"})
-  corsMethods := handlers.AllowedMethods([]string{"POST", "GET", "OPTIONS", "PUT", "DELETE"})
-  corsHeadersExpose := handlers.ExposedHeaders([]string{"Authorization"})
-  corsMaxAge := handlers.MaxAge(24 * 60 * 60)
-
-  handlerFunc := func(w http.ResponseWriter, r *http.Request) {
-    if !strings.HasPrefix(r.URL.Path, "/api") {
-      // we handle requests only if there is "/api" prefix, otherwise return 404
-      http.NotFound(w, r)
-      return
-    }
-
-    if wrappedGrpc.IsGrpcWebRequest(r) {
-      // if request has grpc fingerprint, then try to handle it as web grpc request
-      // we need to trim "/api" prefix from url to handle grpc correctly
-      r.URL.Path = strings.TrimPrefix(r.URL.Path, "/api")
-      wrappedGrpc.ServeHTTP(w, r)
-      return
-    }
-
-    // fall back to other servers
-    handlers.CORS(corsOrigins, corsHeaders, corsMethods, corsHeadersExpose, corsMaxAge)(router).ServeHTTP(w, r)
-  }
-
-  httpSrv := &http.Server{
-    Addr:    fmt.Sprintf("%s:%s", conf.httpHost, conf.httpPort),
-    Handler: http.HandlerFunc(handlerFunc),
-  }
-
-  // if any element of group will exit with error,
-  // then the other elements will be interrupted
-  var g oklog.Group
-  {
-    g.Add(func() error {
-      // http server
-      fmt.Println("http server listen on ", httpSrv.Addr)
-      return httpSrv.ListenAndServe()
-    }, func(err error) {
-      fmt.Println("http server stopping gracefully due to error: ", err)
-      ctxShutDown, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-      defer func() {
-        cancel()
-      }()
-
-      if err = httpSrv.Shutdown(ctxShutDown); err != nil && err != http.ErrServerClosed {
-        log.Println("failed to shutdown server gracefully: ", err)
-      }
-    })
-  }
-  //{
-  //  lis, err := net.Listen("tcp", ":8844")
-  //  if err != nil {
-  //    return err
-  //  }
-  //  g.Add(func() error {
-  //    // GRPC server
-  //    return grpcServer.Serve(lis)
-  //  }, func(err error) {
-  //    lis.Close()
-  //  })
-  //}
-  {
-    c := make(chan os.Signal)
-    g.Add(func() error {
-      signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
-      err := fmt.Errorf("%s", <-c)
-      fmt.Println("received signal: ", err)
-      return err
-    }, func(err error) {
-      close(c)
-    })
-  }
-
-  return g.Run()
+var corsAllowedHeaders = []string{
+	"Accept",
+	"Content-Type",
+	"Content-Length",
+	"Accept-Encoding",
+	"Authorization",
+	"X-CSRF-Token",
+	"array",
+	"arrayindexoffset_",
+	"convertedprimitivefields_",
+	"messageid_",
+	"pivot_",
+	"wrappers_",
+	"x-grpc-web",
 }
 
 func main() {
-  flag.Parse()
+	flag.Parse()
 
-  if err := run(); err != nil {
-    fmt.Println(err)
-  }
+	if err := run(); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func run() error {
+	conf := getConfig()
+
+	grpcServer := grpc.NewServer()
+	proto.RegisterHelloServiceServer(grpcServer, hellosvc.NewServiceServer())
+	wrappedGrpc := grpcweb.WrapServer(grpcServer, grpcweb.WithOriginFunc(func(origin string) bool {
+		return origin == conf.originsAllowed
+	}))
+
+	router := mux.NewRouter().PathPrefix("/api").Subrouter()
+
+	// here you can add more routes and handlers
+	{
+		// router.Methods("GET").Path("/healthcheck").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		//  w.WriteHeader(http.StatusOK)
+		//  w.Write([]byte("ok"))
+		// })
+	}
+
+	corsOrigins := handlers.AllowedOrigins([]string{conf.originsAllowed})
+	corsHeaders := handlers.AllowedHeaders(corsAllowedHeaders)
+	corsMethods := handlers.AllowedMethods([]string{
+		"POST",
+		"GET",
+		"OPTIONS",
+		"PUT",
+		"DELETE",
+	})
+	corsHeadersExpose := handlers.ExposedHeaders([]string{"Authorization"})
+	corsMaxAge := handlers.MaxAge(24 * 60 * 60)
+
+	handlerFunc := func(w http.ResponseWriter, r *http.Request) {
+		if !strings.HasPrefix(r.URL.Path, "/api") {
+			// we handle requests only if there is "/api" prefix, otherwise return 404
+			http.NotFound(w, r)
+			return
+		}
+
+		if wrappedGrpc.IsGrpcWebRequest(r) {
+			// if request has grpc fingerprint, then try to handle it as web grpc request
+			// we need to trim "/api" prefix from url to handle grpc correctly
+			r.URL.Path = strings.TrimPrefix(r.URL.Path, "/api")
+			wrappedGrpc.ServeHTTP(w, r)
+			return
+		}
+
+		// fall back to other servers
+		handlers.CORS(corsOrigins, corsHeaders, corsMethods, corsHeadersExpose, corsMaxAge)(router).ServeHTTP(w, r)
+	}
+
+	httpSrv := &http.Server{
+		Addr:    fmt.Sprintf("%s:%s", conf.httpHost, conf.httpPort),
+		Handler: http.HandlerFunc(handlerFunc),
+	}
+
+	// if any element of group will exit with error,
+	// then the other elements will be interrupted
+	var g oklog.Group
+	{
+		g.Add(func() error {
+			// http server
+			fmt.Println("http server listen on ", httpSrv.Addr)
+			return httpSrv.ListenAndServe()
+		}, func(err error) {
+			fmt.Println("http server stopping gracefully due to error: ", err)
+			ctxShutDown, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+			defer func() {
+				cancel()
+			}()
+
+			if err = httpSrv.Shutdown(ctxShutDown); err != nil && err != http.ErrServerClosed {
+				log.Println("failed to shutdown server gracefully: ", err)
+			}
+		})
+	}
+	// {
+	//  lis, err := net.Listen("tcp", ":8844")
+	//  if err != nil {
+	//    return err
+	//  }
+	//  g.Add(func() error {
+	//    // GRPC server
+	//    return grpcServer.Serve(lis)
+	//  }, func(err error) {
+	//    lis.Close()
+	//  })
+	// }
+	{
+		c := make(chan os.Signal)
+		g.Add(func() error {
+			signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+			err := fmt.Errorf("%s", <-c)
+			fmt.Println("received signal: ", err)
+			return err
+		}, func(err error) {
+			close(c)
+		})
+	}
+
+	return g.Run()
 }
--- template/server/go.mod
+++ template/server/go.mod
@@ -1,18 +1,26 @@
 module server
 
-go 1.16
+go 1.19
 
 require (
-	github.com/desertbit/timer v0.0.0-20180107155436-c41aec40b27f // indirect
-	github.com/golang/protobuf v1.5.0
+	github.com/golang/protobuf v1.5.2
 	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
-	github.com/improbable-eng/grpc-web v0.14.0
-	github.com/mwitkow/go-conntrack v0.0.0-20190716064945-2f068394615f // indirect
+	github.com/improbable-eng/grpc-web v0.15.0
 	github.com/oklog/run v1.1.0
+	google.golang.org/grpc v1.53.0
+	google.golang.org/protobuf v1.28.1
+)
+
+require (
+	github.com/cenkalti/backoff/v4 v4.1.1 // indirect
+	github.com/desertbit/timer v0.0.0-20180107155436-c41aec40b27f // indirect
+	github.com/felixge/httpsnoop v1.0.1 // indirect
+	github.com/klauspost/compress v1.11.7 // indirect
 	github.com/rs/cors v1.7.0 // indirect
 	golang.org/x/net v0.7.0 // indirect
-	google.golang.org/grpc v1.37.0
-	google.golang.org/protobuf v1.26.0
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+	google.golang.org/genproto v0.0.0-20230110181048-76db0878b65f // indirect
 	nhooyr.io/websocket v1.8.7 // indirect
 )
